0d4fb916ebb425fb25b5982f43e629c82858a9f8
Abdullahi Eid <iam@abdullahi.dev>
2024-11-03 00:52:20 -0400

    completed front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {ContactComponent} from "./contact/contact.component";
@@ -7,9 +7,15 @@ import {BookComponent} from "./books/book/book.component";
 import {LoginComponent} from "./login/login.component";
 import {AdminComponent} from "./admin/admin.component";
 import {loggedInGuard} from "./logged-in.guard";
+import {AuthorsComponent} from "./authors/authors.component";
+import {AuthorComponent} from "./authors/author/author.component";
 
 const booksRoutes: Routes = [
   {path: ':id', component: BookComponent}
+];
+
+const authorsRoutes: Routes = [
+  {path: ':id', component: AuthorComponent}
 ];
 
 export const routes: Routes = [
@@ -25,6 +31,8 @@ export const routes: Routes = [
   {path: 'books', component: BooksComponent,
     children: booksRoutes
   },
+  {path: 'authors', component: AuthorsComponent,
+    children: authorsRoutes},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: HomeComponent}
 ];
